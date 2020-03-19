b6bd7658df137e32543915ede2e3ac2dc3d354bf
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-03-19 07:20:11 -0300

    Criando classe Vetor e teste da estrutura de dados

---
+++ src/me/pimpao/estruturadados/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package me.pimpao.estruturadados.vetor;
+
+public class Vetor {
+	
+	private String[] elementos;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+}
---
+++ src/me/pimpao/estruturadados/vetor/testes/TesteVetor.java
@@ -0,0 +1,13 @@
+package me.pimpao.estruturadados.vetor.testes;
+
+import me.pimpao.estruturadados.vetor.Vetor;
+
+public class TesteVetor {
+	
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+	}
+}
